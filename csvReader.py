85447cbcd16d0f56d5a1efcdb498c198a47a2b2b
lanceDamage <heath.nieddu@outlook.com>
2018-01-29 22:59:10 +0000

    Update csvReader.py

--- csvReader.py
+++ csvReader.py
@@ -1,10 +1,10 @@
 '''
-In this project, I'll import some simple data from local location within the spyder file
+In this py3 recipe, I'll import some simple data from local location within the spyder file
 '''
 #first, I'll import some important tools
 import csv
 #this will format the csvcar
 Reader = csv.reader(open('mycardata.csv',newline=''),delimiter=' ')
 #now I will iterate through the csv
-for row in carReader:    
+for row in carReader:
     print(', '.join(row))
